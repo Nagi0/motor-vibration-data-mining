a600ad13e0f23d8b14ae469e9b78499fa075eab8
Nagi0 <brunolefonseca13@gmail.com>
2024-12-19 01:03:23 -0300

    feat: update phase3

--- motorvibration/humanAttempt/modeling/Model.py
+++ motorvibration/humanAttempt/modeling/Model.py
@@ -1,10 +1,21 @@
+import os
+from ast import literal_eval
 from dataclasses import dataclass
+from dotenv import load_dotenv
+import numpy as np
 import polars as pl
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
+import umap
 from sklearn.cluster import DBSCAN
-from sklearn.decomposition import PCA
-from sklearn.metrics import adjusted_rand_score, normalized_mutual_info_score, v_measure_score
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import (
+    adjusted_rand_score,
+    normalized_mutual_info_score,
+    v_measure_score,
+    davies_bouldin_score,
+    silhouette_score,
+    calinski_harabasz_score,
+)
 import plotly.express as px
 
 
@@ -13,10 +24,20 @@ class Model:
     dataset_path: str
 
     def filter_motor_operation(self, p_df: pl.DataFrame, p_name: str) -> pl.DataFrame:
-        return p_df.lazy().filter(pl.col("file_name").str.contains(f"{p_name}")).collect()
+        return p_df.lazy().filter(pl.col("label") == p_name).collect()
 
     def filter_not_motor_operation(self, p_df: pl.DataFrame, p_name: str) -> pl.DataFrame:
-        return p_df.lazy().filter(~pl.col("file_name").str.contains(f"{p_name}")).collect()
+        return p_df.lazy().filter(pl.col("label") != p_name).collect()
+
+    def create_class_labels(self, p_df: pl.DataFrame, p_labels_name_list: list) -> np.array:
+        labeled_df_list = []
+
+        for label_name in p_labels_name_list:
+            df = p_df.lazy().filter(pl.col("file_name").str.contains(f"{label_name}")).collect()
+            df = df.with_columns(pl.lit(label_name).alias("label"))
+            labeled_df_list.append(df)
+
+        return pl.concat(labeled_df_list)
 
     def load_dataset(self):
         return pl.read_csv(self.dataset_path)
@@ -33,8 +54,11 @@ class Model:
 
 
 if __name__ == "__main__":
+    load_dotenv("motorvibration/config/.env")
     model = Model("motorvibration/Data/motor_vibration_dataset.csv")
     dataset = model.load_dataset()
+
+    dataset = model.create_class_labels(dataset, literal_eval(os.environ["labels_list"]))
 
     normal_operation_df = model.filter_motor_operation(dataset, "normal")
     normal_operation_df = normal_operation_df.with_columns(pl.lit(True).alias("normal_operation"))
@@ -45,12 +69,9 @@ if __name__ == "__main__":
     all_motor_operations_df = pl.concat([normal_operation_df, not_normal_operation_df])
     print(all_motor_operations_df)
 
-    # Escolher os harmônicos e frequências para o scatter plot
     harmonic = 1
     x_column = f"overhang_3_harmonic_{harmonic}_frequency"
     y_column = f"overhang_3_harmonic_{harmonic}"
-
-    # Criar scatter plot interativo
     fig = px.scatter(
         all_motor_operations_df,
         x=x_column,
@@ -60,32 +81,91 @@ if __name__ == "__main__":
         title=f"Scatter Plot Interativo: {x_column} vs {y_column}",
         labels={"normal_operation": "Operação Normal"},
     )
-
-    # Configurar layout do gráfico
     fig.update_traces(marker=dict(size=8, opacity=0.7, line=dict(width=0.5, color="DarkSlateGrey")))
     fig.update_layout(
         xaxis_title=x_column,
         yaxis_title=y_column,
         legend_title="Operação",
     )
-
-    # Exibir o gráfico
     fig.show()
 
-    training_data = all_motor_operations_df.drop(["file_name", "normal_operation"]).to_pandas()
+    training_data = all_motor_operations_df.drop(["file_name", "label", "normal_operation"]).to_pandas()
 
     scaler, normal_data_scaled = model.normalize_data(
-        normal_operation_df.drop(["file_name", "normal_operation"]).to_pandas()
+        normal_operation_df.drop(["file_name", "label", "normal_operation"]).to_pandas()
     )
     training_data_scaled = model.apply_normalizer(training_data, scaler)
 
-    print(scaler)
-    print(normal_data_scaled.shape)
+    reducer = umap.UMAP(metric="manhattan", random_state=42)
+    embeddings = reducer.fit_transform(
+        X=training_data_scaled, y=all_motor_operations_df["normal_operation"].to_numpy()
+    )
 
-    kmeans = DBSCAN(eps=30.0)
-    y_pred = kmeans.fit_predict(training_data_scaled)
+    plot_data = pd.DataFrame(embeddings, columns=["Component1", "Component2"])
+    plot_data["Label"] = all_motor_operations_df["label"].to_numpy()
+
+    fig = px.scatter(
+        plot_data,
+        x="Component1",
+        y="Component2",
+        color="Label",
+        title="Anomalias Detectadas com GMM - PCA 3D",
+        labels={"Anomaly": "É Anômalo", "Label": "Rótulo Original"},
+    )
+
+    fig.update_traces(marker=dict(size=6, opacity=1.0))
+    fig.update_layout(scene=dict(xaxis_title="PCA1", yaxis_title="PCA2"))
+    fig.show()
+
+    dbscan = DBSCAN(eps=30.0)
+    y_pred = dbscan.fit_predict(training_data_scaled)
+
+    ari = adjusted_rand_score(all_motor_operations_df["normal_operation"].to_numpy(), y_pred)
+    nmi = normalized_mutual_info_score(all_motor_operations_df["normal_operation"].to_numpy(), y_pred)
+    v_measure = v_measure_score(all_motor_operations_df["normal_operation"].to_numpy(), y_pred)
+
+    print("Internal Metrics: \n")
+    print("Adjusted Rand Index (ARI):", ari)
+    print("Normalized Mutual Information (NMI):", nmi)
+    print("V-Measure:", v_measure)
+
+    print("External Metrics: \n")
+    davies_bouding = davies_bouldin_score(training_data_scaled, y_pred)
+    silhouette = silhouette_score(training_data_scaled, y_pred)
+    calinski = calinski_harabasz_score(training_data_scaled, y_pred)
+    print(f"davies_bouldin_score: {davies_bouding}")
+    print(f"silhouette_score: {silhouette}")
+    print(f"calinski_harabasz_score: {calinski}")
+
+    plot_data = pd.DataFrame(embeddings, columns=["Component1", "Component2"])
+    plot_data["Label"] = all_motor_operations_df["normal_operation"].to_numpy()
+    fig = px.scatter(
+        plot_data,
+        x="Component1",
+        y="Component2",
+        color="Label",
+        title="Anomalias Detectadas com GMM - PCA 3D",
+        labels={"Label": "Rótulo Original"},
+    )
+    fig.update_traces(marker=dict(size=6, opacity=1.0))
+    fig.update_layout(scene=dict(xaxis_title="Component1", yaxis_title="Component2"))
+    fig.show()
+
+    plot_data["Label"] = list(map(str, y_pred))
+    fig = px.scatter(
+        plot_data,
+        x="Component1",
+        y="Component2",
+        color="Label",
+        title="Anomalias Detectadas com GMM - PCA 3D",
+        labels={"Anomaly": "É Anômalo", "Label": "Rótulo Agrupamento"},
+    )
+    fig.update_traces(marker=dict(size=6, opacity=1.0))
+    fig.update_layout(scene=dict(xaxis_title="PCA1", yaxis_title="PCA2"))
+    fig.show()
+
+    y_pred = dbscan.fit_predict(training_data_scaled)
     y_pred[y_pred == 0.0] = 1.0
-    y_pred[y_pred == 2.0] = -1.0
 
     ari = adjusted_rand_score(all_motor_operations_df["normal_operation"].to_numpy(), y_pred)
     nmi = normalized_mutual_info_score(all_motor_operations_df["normal_operation"].to_numpy(), y_pred)
@@ -95,53 +175,46 @@ if __name__ == "__main__":
     print("Normalized Mutual Information (NMI):", nmi)
     print("V-Measure:", v_measure)
 
-    # Criar DataFrame para facilitar o uso com Plotly
-    pca = PCA(n_components=2)
-    reduced_data_3d = pca.fit_transform(training_data_scaled)
-
-    plot_data = pd.DataFrame(reduced_data_3d, columns=["PCA1", "PCA2"])
-    plot_data["Label"] = all_motor_operations_df["normal_operation"].to_numpy()
-
-    # Plotar com Plotly
+    plot_data["Label"] = list(map(str, y_pred))
     fig = px.scatter(
         plot_data,
-        x="PCA1",
-        y="PCA2",
-        # z="PCA3",
-        color="Label",
-        title="Anomalias Detectadas com GMM - PCA 3D",
-        labels={"Anomaly": "É Anômalo", "Label": "Rótulo Original"},
-        color_discrete_map={True: "blue", False: "red"},  # Cores para normal (azul) e anômalo (vermelho)
-    )
-
-    # Configurar layout
-    fig.update_traces(marker=dict(size=6, opacity=1.0))
-    fig.update_layout(
-        scene=dict(xaxis_title="PCA1", yaxis_title="PCA2"), template="plotly_dark"
-    )  # , zaxis_title="PCA3"))
-
-    # Mostrar o gráfico
-    fig.show()
-
-    plot_data["Label"] = y_pred
-
-    # Plotar com Plotly
-    fig = px.scatter(
-        plot_data,
-        x="PCA1",
-        y="PCA2",
-        # z="PCA3",
+        x="Component1",
+        y="Component2",
         color="Label",
         title="Anomalias Detectadas com GMM - PCA 3D",
         labels={"Anomaly": "É Anômalo", "Label": "Rótulo Agrupamento"},
-        color_continuous_scale=px.colors.sequential.Viridis,  # Cores para normal (azul) e anômalo (vermelho)
     )
 
-    # Configurar layout
     fig.update_traces(marker=dict(size=6, opacity=1.0))
-    fig.update_layout(
-        scene=dict(xaxis_title="PCA1", yaxis_title="PCA2"), template="plotly_dark"
-    )  # , zaxis_title="PCA3"))
+    fig.update_layout(scene=dict(xaxis_title="PCA1", yaxis_title="PCA2"))
 
-    # Mostrar o gráfico
+    fig.show()
+
+    for eps in range(30, 150, 1):
+        dbscan = DBSCAN(eps=eps)
+        y_pred = dbscan.fit_predict(training_data_scaled)
+
+        print(f"Eps: {eps}")
+        davies_bouding = davies_bouldin_score(training_data_scaled, y_pred)
+        silhouette = silhouette_score(training_data_scaled, y_pred)
+        calinski = calinski_harabasz_score(training_data_scaled, y_pred)
+        print(f"davies_bouldin_score: {davies_bouding}")
+        print(f"silhouette_score: {silhouette}")
+        print(f"calinski_harabasz_score: {calinski}")
+
+    dbscan = DBSCAN(eps=69)
+    y_pred = dbscan.fit_predict(training_data_scaled)
+    plot_data["Label"] = list(map(str, y_pred))
+    fig = px.scatter(
+        plot_data,
+        x="Component1",
+        y="Component2",
+        color="Label",
+        title="Anomalias Detectadas com GMM - PCA 3D",
+        labels={"Anomaly": "É Anômalo", "Label": "Rótulo Agrupamento"},
+    )
+
+    fig.update_traces(marker=dict(size=6, opacity=1.0))
+    fig.update_layout(scene=dict(xaxis_title="PCA1", yaxis_title="PCA2"))
+
     fig.show()
